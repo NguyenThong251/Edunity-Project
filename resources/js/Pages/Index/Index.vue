42b1ef7b465f7fe595dc34a77360314b8a98fcfd
NguyenThong251 <ht01252004@gmail.com>
2024-09-07 21:11:25 -0700

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>Hello {{ counter }}!
+
+
+    <div class="text-dark-800 mt-20">hello</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+setInterval(() => counter.value++, 1000);
+</script>
